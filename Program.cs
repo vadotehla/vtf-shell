f6a58613810572ffbe528accb83a0fcee3c53a87
i <i@u1t.dev>
2026-07-05 16:47:47 -0400

    add more stuff in

--- Program.cs
+++ Program.cs
@@ -1,1 +1,63 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Global Variables
+
+string input = "";
+bool cont = true;
+List<string> Builtins = new(["exit", "type"]);
+
+static string[] ParseInput(string inp)
+{
+    return inp.Split(" ");
+}
+
+/* Commands to be implemented:
+    - exit | quit (exits)
+    - type (checks location)
+    - open (opens a file)
+    - list (lists files in the working directory)
+    - 
+*/
+while (cont)
+{
+    Console.Write("~ ");
+
+    input = Console.ReadLine();
+    string[] Parsed = ParseInput(input.ToLower());
+
+    if (Parsed.Length > 0)
+    {
+        string command = Parsed.ElementAt(0);
+        switch (command)
+        {
+            case "quit":
+            case "exit":
+                cont = false;
+                break;
+            case "type":
+                {
+                    if (Parsed.Length > 1)
+                    {
+                        string arg = Parsed.ElementAt(1);
+                        if (Builtins.Contains(arg))
+                        {
+                            Console.WriteLine($"{arg} is builtin");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{arg} unknown");
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("type expected 1 argument (command)");
+                        break;
+                    }
+                }
+            default:
+                Console.WriteLine($"{command} unknown");
+                break;
+        }
+    }
+    else continue;
+}
