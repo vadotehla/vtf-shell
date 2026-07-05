f549cdd713fccd6601153223f4a52701f8ee7a5f
i <i@u1t.dev>
2026-07-05 17:40:33 -0400

    add user and stuff and move painting to the main file because the fucking c# language server doesn't fucking work

--- Program.cs
+++ Program.cs
@@ -3,7 +3,8 @@ using System.Text.Json;
 // Global Variables
 
 string input = "";
-string username = null;
+string? usn = null;
+string? workspace = null;
 bool cont = true;
 List<string> Builtins = new(["exit", "type"]);
 FrozenDictionary<string, int> Users = JsonSerializer.Deserialize<Dictionary<string, int>>("""
@@ -18,6 +19,21 @@ static string[] ParseInput(string inp)
     return inp.Split(" ");
 }
 
+static string Paint(string text, int? fg = null, int? bg = null, bool bold = false, bool underline = false)
+{
+    var codes = new List<string>();
+
+    if (bold) codes.Add("1");
+    if (underline) codes.Add("4");
+    if (fg.HasValue) codes.Add(fg.Value.ToString());
+    if (bg.HasValue) codes.Add(bg.Value.ToString());
+
+    if (codes.Count == 0)
+        return text;
+
+    return $"\x1b[{string.Join(";", codes)}m{text}\x1b[0m";
+}
+
 /* Commands to be implemented:
     - exit | quit (exits)
     - type (checks location)
@@ -28,7 +44,7 @@ static string[] ParseInput(string inp)
 */
 while (cont)
 {
-    Console.Write($"|{username ?? "guest"}|>> (no-workspace) ");
+    Console.Write($"|{Paint(usn ?? "guest", fg = 36)}|>> ({Paint(workspace ?? "no-workspace", fg = 31)}) ");
 
     input = Console.ReadLine();
     string[] Parsed = ParseInput(input.ToLower());
@@ -67,15 +83,16 @@ while (cont)
             case "whoami":
                 {
                     Console.Write("|admin.bot|>> ");
-                    if (typeof(username) == "string" && Users.Contains(username))
+                    if (usn is not null && Users.ContainsKey(usn))
                     {
-                        int clearance = Users.GetValueOrDefault(username, 0);
-                        Console.WriteLine($"You are logged in as {username}, with a clearance of {clearance}.");
+                        int clearance = Users.GetValueOrDefault(usn, 0);
+                        Console.WriteLine($"You are logged in as {usn}, with a clearance of {clearance}.");
                     }
                     else
                     {
                         Console.WriteLine("You are not logged in or you are not a valid user.");
                     }
+                    break;
                 }
             default:
                 Console.WriteLine($"{command} unknown");
